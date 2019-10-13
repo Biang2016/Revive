38ee8c56b36f065851de8d18977b2f8c98130ce5
biang-mac <Biang@Biang-PC.(none)>
2019-10-13 00:06:21 -0400

    first stage cave full model completed. Bloom effect

---
+++ Assets/Scripts/Basic/ClientUtil.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class ClientUtil
+{
+    public static Color HTMLColorToColor(string htmlColor)
+    {
+        Color cl = new Color();
+        ColorUtility.TryParseHtmlString(htmlColor, out cl);
+        return cl;
+    }
+}
--- Assets/Scripts/PlayerControll/Player.cs
+++ Assets/Scripts/PlayerControll/Player.cs
@@ -5,8 +5,5 @@ public class Player : MonoBehaviour
     private void Start()
     {
         AudioManager.Instance.BGMFadeIn("bgm/Stream");
-
-        RenderSettings.fog = true;
-        RenderSettings.fogDensity = 1.0f;
     }
 }
