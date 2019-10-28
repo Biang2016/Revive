1fd42d332532028e7cad4b04be92025f13728d73
biang-mac <Biang@Biang-PC.(none)>
2019-10-28 00:02:09 -0400

    start UI

---
+++ Assets/Scripts/Basic/UIPanels/StartMenuPanel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class StartMenuPanel : BaseUIForm
+{
+
+    void Awake()
+    {
+        UIType.IsClearStack = true;
+        UIType.IsClickElsewhereClose = false;
+        UIType.IsESCClose = false;
+        UIType.UIForm_LucencyType = UIFormLucencyTypes.ImPenetrable;
+        UIType.UIForms_ShowMode = UIFormShowModes.Normal;
+        UIType.UIForms_Type = UIFormTypes.Normal;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
